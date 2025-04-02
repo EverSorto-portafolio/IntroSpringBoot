5628e2ad0f15783f8a0feaa4d4a1004a4fb217ef
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-04-01 23:35:06 -0600

    guia  2  se finaliza el aplicando controladores

---
+++ src/main/java/com/introduccion/springnott/web/springboot_web/controllers/UserRestController.java
@@ -0,0 +1,22 @@
+package com.introduccion.springnott.web.springboot_web.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UserRestController {
+
+  @GetMapping("/detalles")
+  public Map<String, Object> detalles() {
+    Map<String, Object> body = new HashMap<>();
+    body.put("title", "Hola desde Spring Boot");
+    body.put("nombre", "Ever Sorto");
+    body.put("apellido", "Ayala");
+    return body;
+  }
+}

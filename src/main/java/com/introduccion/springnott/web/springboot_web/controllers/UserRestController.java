55efc7b6589127538b6d1c149256c8a8fe12d713
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-04-02 22:15:29 -0600

    Gua 2 Uso de @RequestMApping

--- src/main/java/com/introduccion/springnott/web/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/introduccion/springnott/web/springboot_web/controllers/UserRestController.java
@@ -3,12 +3,13 @@ package com.introduccion.springnott.web.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
   @GetMapping("/detalles")

c223a8105a36681dd6651443ceac389e3286dd69
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2025-04-22 22:01:14 -0600

    guia 4 se crea el html lista y el controlador

--- src/main/java/com/introduccion/springnott/web/springboot_web/controllers/UserController.java
+++ src/main/java/com/introduccion/springnott/web/springboot_web/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.introduccion.springnott.web.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.introduccion.springnott.web.springboot_web.controllers.models.User;
@@ -21,13 +25,18 @@ public String getMethodName(Model model) {
     identificarUsuario,
     "correo@servidor");
 
-
     model.addAttribute(
         "userDTO", 
         dtoEmailUserTitleCorreo);
     return "detalles2";
 }
 
-
-
+@GetMapping("/lista")
+public String lista(ModelMap model){
+    List<User> user = new ArrayList<>();
+    user.add(new User("Ever", "Sorto"));
+    model.addAttribute("user", user);
+    model.addAttribute("title", "Lista de usuarios");
+    return "lista"; 
+}
 }
